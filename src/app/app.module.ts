e0ff5321a30fa7163f446a0fe7e6ff21f5e8e8d1
SauleAld <ST.ALDABERGENOVA@GMAIL.COM>
2022-01-14 12:19:20 -0600

    January 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -28,6 +28,7 @@ import {
 import myappConfig from './config/myapp-config';
 import { MembersPageComponent } from './components/members-page/members-page.component';
 import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { AuthInterceptorService } from './service/auth-interceptor.service';
 
 const oktaConfig = Object.assign({
   onAuthRequired: (injector) => {
@@ -80,7 +81,8 @@ const routes: Routes = [
     OktaAuthModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [{ provide: OKTA_CONFIG, useValue: oktaConfig}],
+  providers: [{ provide: OKTA_CONFIG, useValue: oktaConfig},
+              {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/config/myapp-config.ts
+++ src/app/config/myapp-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa3g2uy4gKU3jiPQ5d7',
         issuer: 'https://dev-11429932.okta.com',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
